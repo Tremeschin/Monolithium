4ba7d6b2dcf5aaa767f86bfe7082395154bc32a3
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-10-10 04:36:16 -0400

    Remove f64::mul_add due performance degradation on CPUs without fma

--- monolithium/monolithium/utils.rs
+++ monolithium/monolithium/utils.rs
@@ -9,13 +9,13 @@ pub fn nearest(num: i32, mul: i32) -> i32 {
 /// - https://en.wikipedia.org/wiki/Smoothstep
 #[inline(always)]
 pub fn fade(t: f64) -> f64 {
-    t * t * t * (t.mul_add(t.mul_add(6.0, -15.0), 10.0))
+    t * t * t * (t * (t * 6.0 - 15.0) + 10.0)
 }
 
 /// Standard linear interpolation function
 #[inline(always)]
 pub fn lerp(t: f64, a: f64, b: f64) -> f64 {
-    t.mul_add(b - a, a)
+    a + t * (b - a)
 }
 
 pub const GRAD_LOOKUP: [(f64, f64, f64); 16] = [
@@ -44,7 +44,7 @@ pub fn grad(hash: u8, x: f64, y: f64, z: f64) -> f64 {
     if cfg!(feature="grad-lookup") {
         unsafe {
             let (cx, cy, cz) = GRAD_LOOKUP.get_unchecked(hash as usize & 0x0F);
-            return cx.mul_add(x, cy.mul_add(y, cz * z));
+            return (cx * x) + (cy * y) + (cz * z);
         }
     } else {
         let h = hash & 0x0F;
